85997d404497f828de639a552b282971cd991ecd
suryasonu7455 <suryasonu917@gmail.com>
2024-09-09 17:06:03 +0530

    send to advik

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import PlayerContextprovider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <PlayerContextprovider>
       <App />
+    </PlayerContextprovider>
     </BrowserRouter>
   </StrictMode>,
 )
